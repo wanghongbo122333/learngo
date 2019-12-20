05638d4a92e09c9f067e7b76e99f30fda2f35160
wanghongbo <hubert.wang@ucloud.cn>
2019-12-20 17:07:50 +0800

    add opa test
    Signed-off-by: wanghongbo <hubert.wang@ucloud.cn>

--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.2
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/wanghongbo122333/learngo v0.0.0-20191120090301-75cb12bf9fa0 // indirect
-	golang.org/x/crypto v0.0.0-20191119213627-4f8c1d86b1ba // indirect
+	github.com/open-policy-agent/opa v0.15.1
+	golang.org/x/text v0.3.2
 )
---
+++ opatest/opa.go
@@ -0,0 +1,44 @@
+package opatest
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/open-policy-agent/opa/rego"
+)
+
+func OpaTest() {
+
+	ctx := context.Background()
+	fmt.Println(os.Args)
+	// Construct a Rego object that can be prepared or evaluated.
+	r := rego.New(
+		rego.Query(os.Args[2]),
+		rego.Load([]string{os.Args[1]}, nil))
+
+	// Create a prepared query that can be evaluated.
+	query, err := r.PrepareForEval(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load the input document from stdin.
+	var input interface{}
+	dec := json.NewDecoder(os.Stdin)
+	dec.UseNumber()
+	if err := dec.Decode(&input); err != nil {
+		log.Fatal(err)
+	}
+
+	// Execute the prepared query.
+	rs, err := query.Eval(ctx, rego.EvalInput(input))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Do something with the result.
+	fmt.Println(rs)
+}
